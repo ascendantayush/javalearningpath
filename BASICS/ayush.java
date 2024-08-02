9abf4141b52e2e2b06c5dbdb3b148cc1470ade2d
Ayush Raj <161280074+ascendantayush@users.noreply.github.com>
2024-08-02 21:27:40 +0530

    Printing messages in java and difference between different printing methos
    It shows the difference between using println and print

---
+++ BASICS/ayush.java
@@ -0,0 +1,12 @@
+public class ayush {
+    public static void main(String[] args) {
+
+        //the println works same as endl in cpp. It moves the cursour to the next line.  
+        //a short trick to write System.out.println() is write "sout" and press tab. Trick to remember this is you can relate it to cout in cpp.
+      
+        System.out.println("This is line number 1 ");
+        System.out.print("This is line number 2 ");
+        System.out.println("This is line number 3 ");
+        System.out.println("This is line number 4 ");
+    }
+}
